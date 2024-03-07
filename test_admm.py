7f5945102de638d9324458890cdfeed6d0dbd9de
Vladimir Malinovskii <galqiwi@gmail.com>
2024-03-08 00:49:40 +0300

    upd

--- test_admm.py
+++ test_admm.py
@@ -1,6 +1,9 @@
 import numpy as np
 import torch
 import functools
+import tqdm
+
+import itertools
 
 from pathlib import Path
 
@@ -55,6 +58,7 @@ def custom_admm(
     n_iters: int = 20,
     rho: float = 1,
     mask_rule: any = None,
+    is_iterative: bool = False,
 ) -> torch.Tensor:
     if mask_rule is None:
         mask_rule = wanda
@@ -85,8 +89,10 @@ def custom_admm(
     U = torch.zeros_like(W)
 
     assert n_iters > 0
+    mask = mask_rule(target=(W + U).T / norm, XTX=XTX_orig, sparsity=sparsity).T
     for itt in range(n_iters):
-        mask = mask_rule(target=(W + U).T / norm, XTX=XTX_orig, sparsity=sparsity).T
+        if is_iterative:
+            mask = mask_rule(target=(W + U).T / norm, XTX=XTX_orig, sparsity=sparsity).T
         Z = (W + U) * mask
         U = U + (W - Z)
         W = XTXinv.matmul(XY + rho * (Z - U))
@@ -125,36 +131,128 @@ def gradient_mask(target: torch.Tensor, XTX: torch.Tensor, sparsity: float) -> t
     return mask
 
 
-def test(target_name: str = 'LLAMA2_TARGET_BASE.pt') -> List[any]:
+def sparsegpt_mask(target: torch.Tensor, XTX: torch.Tensor, sparsity: float) -> torch.Tensor:
+    out_size, in_size = target.shape
+    assert XTX.shape == (in_size, in_size)
+
+    XTXInvDiag = torch.diag(torch.inverse(XTX))
+    assert XTXInvDiag.shape == (in_size,)
+
+    XTXDiag = torch.diag(XTX)
+    assert XTXDiag.shape == (in_size,)
+
+    errors = (target ** 2) / XTXInvDiag
+
+    top_elems = torch.topk(
+        errors.flatten(),
+        k=int(errors.numel() * sparsity),
+        largest=False,
+    ).indices
+    mask = torch.ones(target.numel(), dtype=torch.bool, device=target.device)
+    mask[top_elems] = 0
+    mask = mask.reshape(target.shape)
+
+    return mask
+
+
+def custom_galqiwi_mask(target: torch.Tensor, XTX: torch.Tensor, sparsity: float) -> torch.Tensor:
+    out_size, in_size = target.shape
+    assert XTX.shape == (in_size, in_size)
+
+    B = target @ XTX
+    assert B.shape == (out_size, in_size)
+
+    A = torch.diag(XTX)
+    assert A.shape == (in_size,)
+
+    errors = (B ** 2) / A
+
+    top_elems = torch.topk(
+        errors.flatten(),
+        k=int(errors.numel() * sparsity),
+        largest=False,
+    ).indices
+    mask = torch.ones(target.numel(), dtype=torch.bool, device=target.device)
+    mask[top_elems] = 0
+    mask = mask.reshape(target.shape)
+
+    return mask
+
+
+def custom_galqiwi_mask_2(target: torch.Tensor, XTX: torch.Tensor, sparsity: float, alpha: float = 0) -> torch.Tensor:
+    out_size, in_size = target.shape
+    assert XTX.shape == (in_size, in_size)
+
+    XTXInvDiag = torch.diag(torch.inverse(XTX))
+    assert XTXInvDiag.shape == (in_size,)
+
+    XTXDiag = torch.diag(XTX)
+    assert XTXDiag.shape == (in_size,)
+
+    errors = (target ** 2) * torch.exp(torch.log(XTXDiag) * (1 - alpha) - torch.log(XTXInvDiag) * alpha)
+
+    top_elems = torch.topk(
+        errors.flatten(),
+        k=int(errors.numel() * sparsity),
+        largest=False,
+    ).indices
+    mask = torch.ones(target.numel(), dtype=torch.bool, device=target.device)
+    mask[top_elems] = 0
+    mask = mask.reshape(target.shape)
+
+    return mask
+
+
+def test(target_name: str = 'LLAMA2_TARGET_BASE.pt', is_iterative: bool = False) -> List[any]:
     results = []
     target_base = get_tensor(target_name)
 
-    exp_name = f'{"admm_reference":<17} | {target_name:<25} |'
+    # target_base = target_base[:1024]
 
-    outliers = admm_reference(target=target_base, XTX=get_XTX(), sparsity=0.99)
-    loss = get_loss(delta_weight=target_base - outliers, XTX=get_XTX()).item()
-    results.append({
-        'name': f'{exp_name} iterative_prune',
-        'loss': loss,
-    })
+    exp_name = f'{"admm_reference":<17} | {target_name:<25} | ' + ('iter' if is_iterative else '')
 
-    exp_name = f'{"admm_custom":<17} | {target_name:<25} |'
-
-    outliers = custom_admm(target=target_base, XTX=get_XTX(), sparsity=0.99)
+    outliers = admm_reference(target=target_base, XTX=get_XTX(), sparsity=0.99, iterative_prune=15 if is_iterative else 0)
     loss = get_loss(delta_weight=target_base - outliers, XTX=get_XTX()).item()
     results.append({
         'name': f'{exp_name}',
         'loss': loss,
     })
 
-    exp_name = f'{"admm_custom_grad":<17} | {target_name:<25} |'
+    exp_name = f'{"admm_custom":<17} | {target_name:<25} | ' + ('iter' if is_iterative else '')
 
-    outliers = custom_admm(target=target_base, XTX=get_XTX(), sparsity=0.99, mask_rule=gradient_mask)
+    outliers = custom_admm(target=target_base, XTX=get_XTX(), sparsity=0.99, is_iterative=is_iterative)
     loss = get_loss(delta_weight=target_base - outliers, XTX=get_XTX()).item()
     results.append({
         'name': f'{exp_name}',
         'loss': loss,
     })
+
+    # exp_name = f'{"admm_custom_grad":<17} | {target_name:<25} | ' + ('iter' if is_iterative else '')
+    #
+    # outliers = custom_admm(target=target_base, XTX=get_XTX(), sparsity=0.99, mask_rule=gradient_mask, is_iterative=is_iterative)
+    # loss = get_loss(delta_weight=target_base - outliers, XTX=get_XTX()).item()
+    # results.append({
+    #     'name': f'{exp_name}',
+    #     'loss': loss,
+    # })
+
+    exp_name = f'{"admm_custom_sgpt":<17} | {target_name:<25} | ' + ('iter' if is_iterative else '')
+
+    outliers = custom_admm(target=target_base, XTX=get_XTX(), sparsity=0.99, mask_rule=sparsegpt_mask, is_iterative=is_iterative)
+    loss = get_loss(delta_weight=target_base - outliers, XTX=get_XTX()).item()
+    results.append({
+        'name': f'{exp_name}',
+        'loss': loss,
+    })
+
+    # exp_name = f'{"admm_custom_galq":<17} | {target_name:<25} | ' + ('iter' if is_iterative else '')
+    #
+    # outliers = custom_admm(target=target_base, XTX=get_XTX(), sparsity=0.99, mask_rule=custom_galqiwi_mask_2, is_iterative=is_iterative)
+    # loss = get_loss(delta_weight=target_base - outliers, XTX=get_XTX()).item()
+    # results.append({
+    #     'name': f'{exp_name}',
+    #     'loss': loss,
+    # })
 
     return results
 
@@ -188,11 +286,33 @@ def main():
     #     plot_rho(target)
 
     results = []
-    for target in targets:
-        results.extend(test(target))
+    for is_iterative, target in tqdm.tqdm(list(itertools.product((False, True), targets))):
+        results.extend(test(target_name=target, is_iterative=is_iterative))
 
     for result in results:
         print(f'{result["name"]:<65} | {result["loss"]}')
+
+
+"""
+admm_reference    | LLAMA2_TARGET_BASE.pt     |                   | 0.04655745510796658
+admm_custom       | LLAMA2_TARGET_BASE.pt     |                   | 0.04610034123178573
+admm_custom_sgpt  | LLAMA2_TARGET_BASE.pt     |                   | 0.049215821234794094
+admm_reference    | LLAMA2_DIFF_BASE.pt       |                   | 0.0007004170255205616
+admm_custom       | LLAMA2_DIFF_BASE.pt       |                   | 0.0006986690087244959
+admm_custom_sgpt  | LLAMA2_DIFF_BASE.pt       |                   | 0.0007511789466704419
+admm_reference    | LLAMA2_DIFF_TUNED.pt      |                   | 0.0009394660339419621
+admm_custom       | LLAMA2_DIFF_TUNED.pt      |                   | 0.0009252707204601611
+admm_custom_sgpt  | LLAMA2_DIFF_TUNED.pt      |                   | 0.0009146751103682076
+admm_reference    | LLAMA2_TARGET_BASE.pt     | iter              | 0.04027909150722395
+admm_custom       | LLAMA2_TARGET_BASE.pt     | iter              | 0.03978602941067679
+admm_custom_sgpt  | LLAMA2_TARGET_BASE.pt     | iter              | 0.04508117530962251
+admm_reference    | LLAMA2_DIFF_BASE.pt       | iter              | 0.0006523642361295563
+admm_custom       | LLAMA2_DIFF_BASE.pt       | iter              | 0.0006505168824564085
+admm_custom_sgpt  | LLAMA2_DIFF_BASE.pt       | iter              | 0.0007074907882695715
+admm_reference    | LLAMA2_DIFF_TUNED.pt      | iter              | 0.0009036392631954713
+admm_custom       | LLAMA2_DIFF_TUNED.pt      | iter              | 0.0008862151520391128
+admm_custom_sgpt  | LLAMA2_DIFF_TUNED.pt      | iter              | 0.0008948043425832325
+"""
 
 
 if __name__ == '__main__':
