1df3ea36028df7c84dbe35f26bcca1d86c12832c
Malinovskii Vladimir <galqiwi@gmail.com>
2024-03-19 11:08:47 +0300

    upd

--- main.py
+++ main.py
@@ -783,8 +783,8 @@ if __name__ == "__main__":
             n_new_params = submodule.in_features * submodule.out_features
             n_non_outlier_bits += n_new_params * submodule.estimate_nbits_per_parameter()
             n_params += n_new_params
-            n_outliers += submodule.outliers_quant.n_outliers
-
+            # n_outliers += submodule.outliers_quant.n_outliers
+    print(model)
     print(n_params)
 
     outliers_param_bits = 0
