de83ef17910b39873868874ae7fc3ef10d15ff9d
Vladimir Malinovskii <galqiwi@gmail.com>
2024-05-07 12:41:01 +0300

    upd

--- src/patched_pickle.py
+++ src/patched_pickle.py
@@ -1,9 +1,11 @@
 import pickle
+import torch.nn as nn
 
 
 class Unpickler(pickle.Unpickler):
     def find_class(self, module, name):
-        print(module, name)
+        if module == 'transformers.activations' and name == 'SiLUActivation':
+            return nn.ReLU
         return super().find_class(module, name)
 
 
