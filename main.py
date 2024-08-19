795eee5dce52251736e65abfde00ebd351d361c2
Malinovskii Vladimir <galqiwi@gmail.com>
2024-08-19 17:22:44 +0300

    wip

--- main.py
+++ main.py
@@ -181,8 +181,6 @@ def quantize_aq(model: PreTrainedModel, data: Sequence, val_data: Optional[Seque
     print("\nStarting AQ quantization ...")
     inps, forward_args = get_inps(model, data, args.model_seqlen, args.devices, args.offload_activations)
 
-    print(forward_args.keys())
-
     outs = [torch.zeros_like(inp_tensor, pin_memory=inp_tensor.is_pinned()) for inp_tensor in inps]
 
     if val_data:
@@ -201,6 +199,9 @@ def quantize_aq(model: PreTrainedModel, data: Sequence, val_data: Optional[Seque
     overall_bits = 0
     number_of_quantized_params = 0
     layers = get_layers(model)
+
+    print(val_inps)
+    print(val_outs)
 
     for layer_index in range(len(layers)):
         print(f"\n---------------- Layer {layer_index} of {len(layers)} ----------------")
