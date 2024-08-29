6ab46626160555b39778b87c675e00d603035d12
Malinovskii Vladimir <galqiwi@gmail.com>
2024-08-29 20:27:17 +0300

    wip

--- eval.py
+++ eval.py
@@ -11,40 +11,25 @@ from src.aq import QuantizedWeight
 
 
 class NoisyLinear(torch.nn.Module):
-    def __init__(self, weight, bias, *, had_block_size = 1024, relative_mse = 0):
+    def __init__(self, weight, bias, *, relative_mse = 0):
         super().__init__()
 
         weight = weight.detach().clone()
         if bias is not None:
             bias = bias.detach().clone()
 
-        self.had_block_size = had_block_size
-
         self.out_features, self.in_features = weight.shape
 
         self.inner = torch.nn.Linear(self.in_features, self.out_features, bias=(bias is not None), dtype=weight.dtype,
                                      device=weight.device)
 
-        assert self.in_features % self.had_block_size == 0, (self.in_features, self.had_block_size)
-        weight = weight.reshape(self.out_features, self.in_features // self.had_block_size, self.had_block_size)
-        weight = weight.reshape(self.out_features, self.in_features)
-
         weight = weight + torch.randn_like(weight) * torch.norm(weight) * (relative_mse ** 0.5) / (weight.numel() ** 0.5)
-
-
 
         self.inner.weight.data = weight
         if bias is not None:
             self.inner.bias.data = bias
 
     def forward(self, input):
-        input_shape = input.shape
-
-        assert input.shape[-1] % self.had_block_size == 0
-
-        input = input.reshape(-1, self.had_block_size)
-        input = hadamard_transform(input, scale=1 / (self.had_block_size ** 0.5))
-        input = input.reshape(input_shape)
 
         return self.inner(input)
 
@@ -71,7 +56,7 @@ def add_noisy_layers(model, relative_mse):
             add_noisy_layers(child, relative_mse)
             continue
 
-        new_linear = NoisyHadamarLinear(child.weight, child.bias, had_block_size=64, relative_mse=relative_mse)
+        new_linear = NoisyLinear(child.weight, child.bias, had_block_size=64, relative_mse=relative_mse)
         setattr(model, child_name, new_linear)
 
     return model
@@ -183,7 +168,7 @@ if __name__ == "__main__":
     child = get_module_by_path(orig_model, args.layer_name)
     assert isinstance(child, torch.nn.Linear)
 
-    new_linear = NoisyHadamarLinear(child.weight, child.bias, had_block_size=64, relative_mse=relative_mse)
+    new_linear = NoisyLinear(child.weight, child.bias, had_block_size=64, relative_mse=relative_mse)
     setattr(parent, layer_name_parts[-1], new_linear)
 
     if args.wandb:
