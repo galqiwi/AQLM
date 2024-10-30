77818b40737a64578d7a1155e3ca68e28f15998f
Malinovskii Vladimir <galqiwi@gmail.com>
2024-10-30 06:44:26 +0300

    upd

--- finetune_fsdp.py
+++ finetune_fsdp.py
@@ -732,7 +732,7 @@ def main():
             for k, v in quantized_weight.state_dict().items()
         })
 
-    assert sorted(quantized_model.state_dict().keys()) == sorted(best_model_state_dict.state_dict().keys())
+    assert sorted(quantized_model.state_dict().keys()) == sorted(best_model_state_dict.keys())
     print('loaded best model state dict')
 
 
