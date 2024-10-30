f5dfbbccf672b7f9ad6a427c12dd633169c1c2e1
Malinovskii Vladimir <galqiwi@gmail.com>
2024-10-30 06:59:07 +0300

    upd

--- finetune_fsdp.py
+++ finetune_fsdp.py
@@ -749,7 +749,7 @@ def main():
             trust_remote_code=args.trust_remote_code,
         )
         args.dataset_name = dataset
-        quantized_model = quantized_model.to('cuda:0')
+        quantized_model = quantized_model.to('cuda:0').to(torch.bfloat16)
         evaluate_perplexity(quantized_model, testloader, args.model_seqlen, device='cuda:0')
 
     return
