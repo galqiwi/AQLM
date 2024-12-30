78d67db66740ed3da438b787445e7070a4161a12
Malinovskii Vladimir <galqiwi@gmail.com>
2024-12-30 13:49:40 +0300

    upd

--- kv-pred.py
+++ kv-pred.py
@@ -66,27 +66,12 @@ class args:  # wannabe argparse namespace
     torch_dtype = "auto"
     compute_dtype = None  # used for computing activations
     dataset = "pajama"
-    total_nsamples = 16 # todo: use larger datasets for actual training
+    total_nsamples = 16  # todo: use larger datasets for actual training
     model_seqlen = 8192
     seed = 42
     offload_activations = True  # if True, store hidden states in RAM
     devices = [device]
     wandb = False  # <-- to be implemented
-
-
-tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_name)
-model = transformers.AutoModelForCausalLM.from_pretrained(
-    args.model_name, torch_dtype=args.torch_dtype, low_cpu_mem_usage=True, use_cache=False
-)
-layers = calibration_utils.get_layers(model)
-
-train_data = calibration_utils.get_loaders(
-    args.dataset,
-    nsamples=args.total_nsamples,
-    seed=args.seed,
-    model_path=args.model_name,
-    seqlen=args.model_seqlen,
-)
 
 
 def update_outs_inplace_(args, layer, inps, outs, **kwargs):
@@ -100,11 +85,13 @@ def update_outs_inplace_(args, layer, inps, outs, **kwargs):
     else:
         return calibration_utils.update_outs_parallel(args.devices, layer, inps, outs, **kwargs)
 
+
 class OutputCatcher(nn.Module):
     def __init__(self, inner):
         super().__init__()
         self.inner = inner
         self.outputs = []
+
     def forward(self, inp, **kwargs):
         output = self.inner(inp)
         self.outputs.append(copy.deepcopy(output))
@@ -151,3 +138,91 @@ def get_dequant_values(old_values, values, pred):
     values_dequantized_tensor = values_delta_dequant_tensor + values_pred_tensor
 
     return list(values_dequantized_tensor)
+
+
+def main():
+    tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_name)
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        args.model_name, torch_dtype=args.torch_dtype, low_cpu_mem_usage=True, use_cache=False
+    )
+    layers = calibration_utils.get_layers(model)
+
+    train_data = calibration_utils.get_loaders(
+        args.dataset,
+        nsamples=args.total_nsamples,
+        seed=args.seed,
+        model_path=args.model_name,
+        seqlen=args.model_seqlen,
+    )
+
+    inps, forward_args = calibration_utils.get_inps(
+        model, train_data, args.model_seqlen, args.devices, args.offload_activations)
+
+    for k, v in forward_args.items():
+        forward_args[k] = v.to(args.devices[0]) if isinstance(v, torch.Tensor) else v
+
+    outs = [torch.zeros_like(inp_tensor, pin_memory=inp_tensor.is_pinned()) for inp_tensor in inps]
+    old_attn_keys = None
+    old_attn_values = None
+
+    predictors_keys = {}
+    predictors_values = {}
+
+    for layer_index in range(len(layers)):
+        print(f"\n---------------- Layer {layer_index} of {len(layers)} ----------------")
+
+        layer_device_original = next(layers[layer_index].parameters()).device
+        layer_dtype_original = next(layers[layer_index].parameters()).dtype
+        layer = layers[layer_index].to(device=args.devices[0], dtype=args.compute_dtype or layer_dtype_original)
+
+        layer.self_attn.k_proj = OutputCatcher(layer.self_attn.k_proj)
+        layer.self_attn.v_proj = OutputCatcher(layer.self_attn.v_proj)
+
+        update_outs_inplace_(args, layer, inps, outs, **forward_args, compute_mse=False)
+
+        attn_keys = layer.self_attn.k_proj.outputs
+        assert all(elem.shape[0] == 1 for elem in attn_keys)
+        attn_keys = [elem[0] for elem in attn_keys]
+
+        attn_values = layer.self_attn.v_proj.outputs
+        assert all(elem.shape[0] == 1 for elem in attn_values)
+        attn_values = [elem[0] for elem in attn_values]
+
+        layer.self_attn.k_proj = layer.self_attn.k_proj.inner
+        layer.self_attn.v_proj = layer.self_attn.v_proj.inner
+
+        layers[layer_index] = layer.to(device=layer_device_original, dtype=layer_dtype_original)
+        del layer
+        torch.cuda.empty_cache()
+
+        inps, outs = outs, inps
+
+        if layer_index == 0:
+            old_attn_keys = attn_keys
+            old_attn_values = attn_values
+            continue
+
+        pred_keys, mse_train_keys, mse_val_keys = get_predictor(old_attn_keys, attn_keys)
+        predictors_keys[layer_index] = pred_keys.cpu()
+        attn_keys = get_dequant_values(old_attn_keys, attn_keys, pred_keys)
+
+        train_bits_keys = - math.log(mse_train_keys) / math.log(4)
+        val_bits_keys = - math.log(mse_val_keys) / math.log(4)
+        print(
+            f'{layer_index=}\tPREDICTOR_KEYS\t{mse_train_keys=:.4f}\t{mse_val_keys=:.4f}\t{train_bits_keys=:.2f}\t{val_bits_keys=:.2f}\t')
+
+        pred_values, mse_train_values, mse_val_values = get_predictor(old_attn_values, attn_values)
+        predictors_values[layer_index] = pred_values.cpu()
+        attn_values = get_dequant_values(old_attn_values, attn_values, pred_values)
+
+        train_bits_values = - math.log(mse_train_values) / math.log(4)
+        val_bits_values = - math.log(mse_val_values) / math.log(4)
+        print(
+            f'{layer_index=}\tPREDICTOR_VALUES\t{mse_train_values=:.4f}\t{mse_val_values=:.4f}\t{train_bits_values=:.2f}\t{val_bits_values=:.2f}\t')
+
+        old_attn_keys = attn_keys
+        old_attn_values = attn_values
+
+
+if __name__ == '__main__':
+    main()
